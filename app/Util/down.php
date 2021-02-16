28d867ef1a7156002c2b6e36b1f07b2f08dce850
sjackson1 <stevenpjackson0@gmail.com>
2021-02-16 14:49:51 -0600

    feat(down): receive data and separate into individual arrays

--- app/Util/down.php
+++ app/Util/down.php
@@ -4,6 +4,7 @@ namespace App\Util;
 
 class Down extends Transfer
 {
+
 	/**
 	* Return endpoint response
 	* @return Body 
@@ -11,6 +12,40 @@ class Down extends Transfer
 	public function getBodyDown()
 	{
 		return $this->responseHandler(self::down()->getBody());
+	}
+
+	/**
+	* Return endpoint keys
+	* @return array
+	*/
+	public  function getKey()
+	{
+		$bodyDown = $this->responseHandler(self::down()->getBody());
+
+		foreach($bodyDown as $array)
+		{
+		  $key = array_keys($array);
+		}
+
+	    return $key;
+
+	}
+	
+	/**
+	* Return endpoint values
+	* @return array
+	*/
+	public function getValue()
+	{
+		$bodyDown = $this->responseHandler(self::down()->getBody());
+
+		foreach($bodyDown[0] as $array=>$key)
+		{
+            $value[] = $key; 
+		}
+
+		return $value; 
+
 	}
 
 }
--- resources/views/includes/raw.blade.php
+++ resources/views/includes/raw.blade.php
@@ -2,19 +2,19 @@
     <div class="card">
         <div class="card-body">
             <h5 class="card-title">Test Server</h5>
-                <p class="card-text">
-                    @php 
-                       echo "<pre>";
-                        print_r($bodyDown[0]['roleId']);
-                       echo "</pre>";
-                       echo "<pre>";
-                        print_r($bodyDown[0]['userId']);
-                       echo "</pre>";
-                       echo "<pre>";
-                        print_r($bodyDown[0]['courseId']);  
-                       echo "</pre>";  
-                    @endphp
-                </p>
+            <p class="card-text">
+                @php 
+                    echo '<pre>';
+                    print_r($bodyDown);
+                    echo '</pre>';
+                    echo '<pre>';
+                    print_r($key);
+                    echo '</pre>'; 
+                    echo '<pre>';
+                    print_r($value);
+                    echo '</pre>';    
+                @endphp
+            </p>
         </div>
     </div>
 </div>
