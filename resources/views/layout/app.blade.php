783dfc2f3a8231c4fe0aa0d8bd783f9b5c4d330c
sjackson1 <stevenpjackson0@gmail.com>
2021-02-16 13:01:39 -0600

    feat(blade): remove function test buttons and update down blade

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -16,8 +16,6 @@
             <nav class="navbar navbar-light bg-light">
             <form class="form-inline">
                 <a href="{{ url('/') }}"><button class="btn btn-outline-info" type="button">Home</button></a>
-                <a href="/enrol"><button class="btn btn-outline-info" type="button">Enrol</button></a>
-                <a href="/unenrol"><button class="btn btn-outline-info" type="button">UnEnrol</button></a>
                 <a href="/down"><button class="btn btn-outline-info" type="button">Down</button></a>
                 <a href="/database"><button class="btn btn-outline-info" type="button">Database</button></a>
             </form>
