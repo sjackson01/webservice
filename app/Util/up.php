c175feb851991ac8fc15964dc028ec80321475f1
sjackson01 <stevenpjackson0@gmail.com>
2021-07-01 14:28:57 -0500

    feat(request): build request urls

--- app/Util/up.php
+++ app/Util/up.php
@@ -36,6 +36,48 @@ class Up Extends Transfer
         return DB::table('functions')->where('functions', '=', $function)->get();
     }
 
+    
+   /**
+    * Organize CSV rows
+    * into columns 
+    * @return Array  
+    */
+
+    public function arrayMapper($array, $index)
+    {
+       array_pop($array); // Remove null 
+
+       foreach($array as $key) // Get array keys
+       {
+           $keys[] = $key; 
+       }
+
+       $exteriorCount = count($keys); // Count top level elements
+       
+       foreach($array as $k => $v)// Get array values
+       {
+           $var[] = $v; 
+       }
+
+       $interiorCount = count($var, 1) - $exteriorCount; // Count Interior Elements  
+
+
+       $single = array_reduce($array, 'array_merge', array());// Creat single array from multi
+
+       $columnHeight = $interiorCount/$exteriorCount; // Get interior array height
+
+       $i=0; 
+       
+       foreach($single as $value) { // Move rows into columns
+            if ($i++ % $columnHeight == $index) {
+                $result[] = $value;
+            } 
+                    
+       }
+
+       return $result; 
+    }
+
     /**
 	 * Return csv values
 	 * @return array
@@ -47,20 +89,29 @@ class Up Extends Transfer
 		$file = scandir($directory, 1); // Get file 
 
 		$open = fopen($directory.$file[0],"r"); // Open File 
-		
+
 		$csv = fgetcsv($open); // Parse File 
 
 		foreach($csv as $key => $value) {
     		$data[] = $value; // Insert columns headers into array
 		}
 
-		$column = array_search('col1', $data); // Select active column header 
-
-		while(! feof($open)){
-    		$columnValues[] = fgetcsv($open)[$column]; // Insert column values into array 
+        while(! feof($open)){
+    		$array[] = fgetcsv($open); // Convert CSV to array
 		}
+        
+        array_pop($array); // Remove null element 
 
-        return $columnValues; // Return csv values
+		$index = array_search($columnHeader, $data); // Select active column header 
+
+        if($index === false)
+        {
+            $index = 0; 
+        } 
+        
+		$result = $this->arrayMapper($array, $index); // Convert CSV rows to columns  
+
+        return $result; // Return csv values
 
 		fclose($open); // Close file 
 	}
@@ -95,64 +146,105 @@ class Up Extends Transfer
             if(isset($value->parameter1))
             {
 			    $csvValue = $this->getCsvParameters($value->parameter1); // Get csv value
-                
-                $url .= $strings->paramstring1; // Add string
-                $url .= $csvValue[0]; // Add csv value
+
+                $url .= $strings->paramstring1.$csvValue[0]; // Add string and csv value
+
+                $height = count($csvValue);
+
+                for($i = 0; $i < $height; $i++)
+                { 
+                    $urls[] = $url; 
+                }
+    
             }
 
             if(isset($value->parameter2))
-            {   
-                $csvValue = $this->getCsvParameters($value->parameter2); 
+            {
+			    $csvValue = $this->getCsvParameters($value->parameter2); // Get csv value
 
-                $url .= $strings->paramstring2; // Add string
-                $url .= $csvValue[1]; // Add csv value
+                $url .= $strings->paramstring2.$csvValue[0]; // Add string and csv values
+
+                foreach($csvValue as $v)
+                { 
+                    $urls[] = $url; 
+                    
+                }
+               
             }
             
             if(isset($value->parameter3))
             {
-                $csvValue = $this->getCsvParameters($value->parameter3); 
+			    $csvValue = $this->getCsvParameters($value->parameter3); // Get csv value
+                
+                $url .= $strings->paramstring3.$csvValue[0]; // Add string and csv value
 
-                $url .= $strings->paramstring3; // Add string
-                $url .= $csvValue[2]; // Add csv value
+                foreach($csvValue as $key)
+                { 
+                    $urls[] = $url; 
+                }
+    
             }
 
-            if(isset($value->paramter4))
+            if(isset($value->parameter4))
             {
-                $csvValue = $this->getCsvParameters($value->parameter4); 
+			    $csvValue = $this->getCsvParameters($value->parameter4); // Get csv value
+                
+                $url .= $strings->paramstring4.$csvValue[0]; // Add string and csv value
 
-                $url .= $strings->paramstring4; // Add string
-                $url .= $csvValue[3]; // Add csv value
+                foreach($csvValue as $key)
+                { 
+                    $urls[] = $url; 
+                }
+    
+            }
+            
+            if(isset($value->parameter5))
+            {
+			    $csvValue = $this->getCsvParameters($value->parameter5); // Get csv value
+                
+                $url .= $strings->paramstring5.$csvValue[0]; // Add string and csv value
+
+                foreach($csvValue as $key)
+                { 
+                    $urls[] = $url; 
+                }
+    
             }
 
             if(isset($value->parameter5))
             {
-                $csvValue = $this->getCsvParameters($value->parameter5); 
+			    $csvValue = $this->getCsvParameters($value->parameter5); // Get csv value
+                
+                $url .= $strings->paramstring6.$csvValue[0]; // Add string and csv value
 
-                $url .= $strings->paramstring5; // Add string
-                $url .= $csvValue[4]; // Add csv value
+                foreach($csvValue as $key)
+                { 
+                    $urls[] = $url; 
+                }
+    
             }
 
             if(isset($value->parameter6))
             {
-                $csvValue = $this->getCsvParameters($value->parameter6); 
+			    $csvValue = $this->getCsvParameters($value->parameter6); // Get csv value
                 
-                $url .= $strings->paramstring6; // Add string
-                $url .= $csvValue[5]; // Add csv value 
+                $url .= $strings->paramstring2.$csvValue[0]; // Add string and csv value
+
+                foreach($csvValue as $key)
+                { 
+                    $urls[] = $url; 
+                }
+    
             }
             
-            if(isset($url))
-            {
-                $url .= $wsformat.$format; // Add format 
-            }
+            $count = count($urls); // Count urls
 
-            $requestUrl = explode(',',$url); // Convert url to array
+            $multiplier = $count/3; // Select urls to send
+        
+            $send = array_splice($urls, -$multiplier); // Remove incompelte urls 
 
-            foreach($requestUrl as $array) // Add urls to array 
-            {
-                $requestUrls[] = $array; 
-            }
-           
-            return $requestUrls; // Return request urls array 
+            return $send; 
+
         }
     
     }  
